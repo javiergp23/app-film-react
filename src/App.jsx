717bff5ee66fc576a1ffac56a0bbf6513473b438
Javier Gonzalez <javgon2306@gmail.com>
2024-10-16 18:09:51 -0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { useState } from 'react'
 import Peliculas from "./componenets/Peliculas.jsx";
 import PageWraper from "./componenets/PageWraper.jsx";
 import peliculasJson from './pelicula.json'
+import Paginacion from "./componenets/Paginacion.jsx";
 
 function App() {
+  const [paginaActual, setPaginaActual] = useState(1);
+  const TOTAL_POR_PAGINA = 5;
 
   let peliculas = peliculasJson;
+
+  const cargarPeliculas = () => {
+    peliculas = peliculas.slice((paginaActual - 1) * TOTAL_POR_PAGINA, paginaActual * TOTAL_POR_PAGINA);
+  };
+
+  const getTotalPaginas = () => {
+    let canntidadTotalDePeliculas = peliculasJson.length
+    Math.ceil(cantidadTotalDePeliculas / TOTAL_POR_PAGINA)
+  };
+
+  cargarPeliculas();
+
   return (
     <>
       <PageWraper>
@@ -22,6 +38,10 @@ function App() {
           >
         </Peliculas>
         )}
+
+      <Paginacion pagina={paginaActual} total={4} onChange={(pagina) => {
+        setPaginaActual(pagina);
+      }}/>
       </PageWraper>
     </>
   );
--- src/componenets/Paginacion.jsx
+++ src/componenets/Paginacion.jsx
@@ -1,7 +1,33 @@
-const Paginacion = () => {
-  return (
-    <div>Paginacion</div>
-  )
-}
+import { useState } from "react";
 
-export default Paginacion
+const Paginacion = (props) => {
+    
+
+    const getPAginas = () => {
+        const resultado = [];
+        for (let i = 0; i < props.total; i++){
+            let pagina = i + 1;
+            resultado.push(
+            <a onClick={() => props.onChange(pagina)} 
+            className={props.pagina === pagina ? 'active' : ''} >{pagina}</a>
+            );
+        }
+        return resultado;
+    }
+   
+  return (
+    <>
+    <div className="topbar-filter">
+      <div className="pagination2">
+        <span>Page {props.pagina} of {props.total}:</span>
+        {getPAginas()}
+        <a>
+          <i className="ion-arrow-right-b"></i>
+        </a>
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default Paginacion;
