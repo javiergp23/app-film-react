a76c43fe5f2d723ca6f28cd9791409035556e728
Javier Gonzalez <javgon2306@gmail.com>
2024-10-17 17:30:58 -0300

    fix fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Peliculas from "./componenets/Peliculas.jsx";
 import PageWraper from "./componenets/PageWraper.jsx";
 // import peliculasJson from './pelicula.json'
@@ -9,24 +9,31 @@ function App() {
   const [peliculas, setPeliculas] = useState([]);
   const TOTAL_POR_PAGINA = 5;
 
+  useEffect(() => {
+    buscarPeliculas()
+  },[peliculas])
   // let peliculas = peliculasJson;
 
   const buscarPeliculas = async () => {
     let url = `https://lucasmoy.dev/data/react/peliculas.json`;
 
-    let resultado = await fetch(url, {
-      "method": "GET",
-      "mode": 'no-cors',
-      "headers": {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-      }
+    let resultado = fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
     })
-    let json = resultado.json();
-    console.log(json)
+      // "method": "GET",
+      // "mode": 'no-cors',
+      // "headers": {
+      //   "Accept": "application/json",
+      //   "Content-Type": "application/json",
+        
+    
+    
+    // let json = resultado.json();
+    // console.log(json)
 
   }
-  buscarPeliculas()
 
   // const cargarPeliculas = () => {
   //   peliculas = peliculas.slice((paginaActual - 1) * TOTAL_POR_PAGINA, paginaActual * TOTAL_POR_PAGINA);
