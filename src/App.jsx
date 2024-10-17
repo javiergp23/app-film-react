4d04fe12c5fdfeb35c36be301d2cc6b805114b5d
Javier Gonzalez <javgon2306@gmail.com>
2024-10-17 16:51:28 -0300

    fix buscarPeliculas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import Peliculas from "./componenets/Peliculas.jsx";
 import PageWraper from "./componenets/PageWraper.jsx";
-import peliculasJson from './pelicula.json'
+// import peliculasJson from './pelicula.json'
 import Paginacion from "./componenets/Paginacion.jsx";
 
 function App() {
   const [paginaActual, setPaginaActual] = useState(1);
+  const [peliculas, setPeliculas] = useState([]);
   const TOTAL_POR_PAGINA = 5;
 
-  let peliculas = peliculasJson;
+  // let peliculas = peliculasJson;
 
   const buscarPeliculas = async () => {
     let url = `https://lucasmoy.dev/data/react/peliculas.json`;
@@ -18,28 +19,33 @@ function App() {
       "mode": 'no-cors',
       "headers": {
         "Accept": "application/json",
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       }
     })
     let json = resultado.json();
-    alert(json)
+    console.log(json)
+
   }
   buscarPeliculas()
 
-  const cargarPeliculas = () => {
-    peliculas = peliculas.slice((paginaActual - 1) * TOTAL_POR_PAGINA, paginaActual * TOTAL_POR_PAGINA);
-  };
+  // const cargarPeliculas = () => {
+  //   peliculas = peliculas.slice((paginaActual - 1) * TOTAL_POR_PAGINA, paginaActual * TOTAL_POR_PAGINA);
+  // };
 
   const getTotalPaginas = () => {
     let canntidadTotalDePeliculas = peliculasJson.length
     Math.ceil(cantidadTotalDePeliculas / TOTAL_POR_PAGINA)
   };
 
-  cargarPeliculas();
+  // cargarPeliculas();
 
   return (
     <>
       <PageWraper>
+
+        <button onClick={buscarPeliculas}>
+        </button>
+      
         {peliculas.map(pelicula => 
 
           <Peliculas
